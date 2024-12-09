95ce249d737f5af272fce2d114d875b46fdb0697
Сергей Грабина <Tyuiu.grabinasa@outlook.com>
2024-12-09 14:54:20 +0500

    Task1

--- Tyuiu.GrabinaSA.Sprint6.Task1.V24.Lib/DataService.cs
+++ Tyuiu.GrabinaSA.Sprint6.Task1.V24.Lib/DataService.cs
@@ -5,18 +5,30 @@ namespace Tyuiu.GrabinaSA.Sprint6.Task1.V24.Lib
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
-            double[] valueArray;
-            int len = (stopValue - startValue) + 1;
-            valueArray = new double[len];
-            double y;
-            int count = 0;
-            for (int x = startValue; x <= stopValue; x++)
+            if (startValue > startValue)
             {
-                y = Math.Round(((3 * x) + 2 - (((2 * x) - x) / (Math.Cos(x) + 1))), 2);
-                valueArray[count] = y;
-                count++;
+                int temp = startValue;
+                stopValue = startValue;
+                startValue = temp;
             }
-            return valueArray;
+
+            double[] resMass = new double[Math.Abs(stopValue - startValue) + 1];
+
+            for (int i = 0; i < resMass.Length; i++)
+            {
+                if (double.IsInfinity(startValue) || double.IsNaN(startValue))
+                {
+                    resMass[i] = 0.0;
+                    startValue++;
+                    continue;
+                }
+
+                resMass[i] = Math.Sin(startValue) / (startValue + 1.7) - Math.Cos(startValue) * 4 * startValue - 6;
+                resMass[i] = Math.Round(resMass[i], 2);
+                startValue++;
+            }
+
+            return resMass;
         }
     }
 }
