8ec75bbe7844a87bf3f901fc6822ece8a60ecf49
Сергей Грабина <Tyuiu.grabinasa@outlook.com>
2024-12-09 15:57:28 +0500

    Task6

--- Tyuiu.GrabinaSA.Sprint6.Task6.V20.Lib/DataService.cs
+++ Tyuiu.GrabinaSA.Sprint6.Task6.V20.Lib/DataService.cs
@@ -3,23 +3,33 @@ namespace Tyuiu.GrabinaSA.Sprint6.Task6.V20.Lib
 {
     public class DataService : ISprint6Task6V20
     {
+        public int len;
         public string CollectTextFromFile(string str, string path)
         {
-            string resStr = "";
-
-            using (StreamReader sr = new StreamReader(path))
+            string strResult;
+            using (StreamReader reader = new StreamReader(path))
             {
                 string line;
-                while ((line = sr.ReadLine()) != null)
+                while ((line = reader.ReadLine()) != "")
                 {
-                    string[] strings = line.Trim().Split(' ');
-                    if (strings.Length > 1)
-                    {
-                        resStr += strings[1] + " ";
-                    }
+                    len++;
                 }
             }
-            return resStr.Trim();
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                int index = 0;
+                string[] res = new string[len];
+                string line;
+                while ((line = reader.ReadLine()) != "")
+                {
+                    string[] words = line.Split(' ');
+                    res[index] = words[1];
+                    index++;
+                }
+                strResult = string.Join(" ", res);
+            }
+            return strResult;
         }
     }
 }
