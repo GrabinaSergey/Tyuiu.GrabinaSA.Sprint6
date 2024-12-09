8a0be292e7b49aa8697d1ca6fecac07801488c78
Сергей Грабина <Tyuiu.grabinasa@outlook.com>
2024-12-09 14:50:56 +0500

    Task1

---
+++ Tyuiu.GrabinaSA.Sprint6.Task1.V24.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.GrabinaSA.Sprint6.Task1.V24.Lib;
+namespace Tyuiu.GrabinaSA.Sprint6.Task1.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            double[] res = ds.GetMassFunction(0, 9);
+            double[] wait = { -6, -7.85, -2.43, 5.91, 4.33, -11.82, -29.08, -27.03, -1.24, 26.84 };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.GrabinaSA.Sprint6.Task1.V24/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.GrabinaSA.Sprint6.Task1.V24
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
