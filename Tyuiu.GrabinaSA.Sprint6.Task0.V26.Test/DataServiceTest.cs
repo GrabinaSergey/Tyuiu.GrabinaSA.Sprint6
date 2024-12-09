86d957192629ce55ea414cfdd7bb113c01ebeca8
sakur <sakur@DIEUMORT>
2024-12-09 14:40:07 +0500

    Add project files.

---
+++ Tyuiu.GrabinaSA.Sprint6.Task0.V26.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.GrabinaSA.Sprint6.Task0.V26.Lib;
+namespace Tyuiu.GrabinaSA.Sprint6.Task0.V26.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 3;
+            double res = ds.Calculate(x);
+            double wait = 0.769;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
