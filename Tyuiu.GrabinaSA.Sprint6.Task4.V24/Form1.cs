2c2a9ce477cfb411ed0cd69d182df20f0d2176a7
Сергей Грабина <Tyuiu.grabinasa@outlook.com>
2024-12-09 15:21:40 +0500

    Task4

---
+++ Tyuiu.GrabinaSA.Sprint6.Task4.V24/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.GrabinaSA.Sprint6.Task4.V24
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
